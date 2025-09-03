c427d982357db63c8e0c2988e4733addf8e36186
João Linux <jplcampanella@gmail.com>
2025-09-03 15:18:23 -0300

    ADD: rotas

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const homeRouter = require('./routes/home');
+const cadastroRouter = require('./routes/cadastro');
+const loginRouter = require('./routes/login');
+const listaUsuariosRouter = require('./routes/lista_usuarios');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +21,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', homeRouter);
+app.use('/cadastro', cadastroRouter);
+app.use('/login', loginRouter);
+app.use('/lista_usuarios', listaUsuariosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
